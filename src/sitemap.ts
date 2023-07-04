7c2e56dfe74af049995753064d385de74c020b01
hyverno <solo.hyverno@gmail.com>
2023-07-04 19:28:41 +0200

    update

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -2,8 +2,8 @@ import type { RO_Sitemap } from 'sveltekit-sitemap';
 
 export const sitemap = (<const>{
    "/": true,
-   "/need-website/seo": true,
-   "/need-website": false
+   "/need-website/seo": false,
+   "/need-website": true
 }) satisfies RO_Sitemap
 
 export type Sitemap = typeof sitemap
